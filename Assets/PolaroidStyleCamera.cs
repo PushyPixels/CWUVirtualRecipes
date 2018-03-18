117de427193463e75cf0984307092604c828eb35
Maximilian Maroe <drakfyre@gmail.com>
2018-03-18 16:46:40 -0700

    Polaroid episode stuff

---
+++ Assets/PolaroidStyleCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolaroidStyleCamera : MonoBehaviour
+{
+    public RenderTexture rt;
+    public Renderer picturePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakePicture()
+    {
+        Debug.Log("Taking picture");
+    }
+}
