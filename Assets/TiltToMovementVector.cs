d34c93762247dab4b956709fdbcc228549234920
Maximilian Maroe <drakfyre@gmail.com>
2018-09-09 22:09:02 -0700

    Done-ish!

--- Assets/TiltToMovementVector.cs
+++ Assets/TiltToMovementVector.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiltToMovementVector : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class TiltToMovementVector : MonoBehaviour
+{
+    public float xTilt;
+    public float zTilt;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+	    // Get X tilt and convert to float
+        Vector3 rightVector = Vector3.ProjectOnPlane(transform.right,Vector3.up).normalized;
+        xTilt = 1.0f - Vector3.Dot(rightVector,transform.right);
+        xTilt *= Mathf.Sign(transform.right.y);
+
+        // Get Z tilt and convert to float
+        Vector3 forwardVector = Vector3.ProjectOnPlane(transform.forward,Vector3.up).normalized;
+        zTilt = 1.0f - Vector3.Dot(forwardVector,transform.forward);
+        zTilt *= Mathf.Sign(transform.forward.y);
 	}
 }
