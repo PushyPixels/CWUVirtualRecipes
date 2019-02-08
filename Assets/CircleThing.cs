748d316a2a686f57cf729792e40ec38cb2d4ab8d
Maximilian Maroe <drakfyre@gmail.com>
2019-02-08 09:20:36 -0800

    CircleThing

---
+++ Assets/CircleThing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleThing : MonoBehaviour
+{
+    public Transform ball;
+    public Vector3 speed = Vector3.one;
+
+    private float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = ball.localPosition;
+        newPosition.x = Mathf.Sin(2f * Mathf.PI * t * speed.x);
+        newPosition.y = Mathf.Cos(2f * Mathf.PI * t * speed.y);
+        newPosition.z = Mathf.Sin(2f * Mathf.PI * t * speed.z);
+        ball.localPosition = newPosition;
+        t += Time.deltaTime;
+    }
+}
