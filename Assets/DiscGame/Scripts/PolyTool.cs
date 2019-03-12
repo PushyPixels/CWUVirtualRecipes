b32cbde14bd217ba4d0e577a8b54de1f669d86bc
Maximilian Maroe <drakfyre@gmail.com>
2019-03-12 16:19:10 -0700

    Basic poly editor test

---
+++ Assets/DiscGame/Scripts/PolyTool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolyTool : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public VertexHandle vertexHandlePrefab;
+    public MidpointHandle midpointHandlePrefab;
+
+    private Dictionary<Vector3,VertexHandle> vertexHandleDictionary = new Dictionary<Vector3,VertexHandle>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        foreach(Vector3 vertex in meshFilter.mesh.vertices)
+        {
+            if(!vertexHandleDictionary.ContainsKey(vertex))
+            {
+                VertexHandle vertexHandle = Instantiate(vertexHandlePrefab,meshFilter.transform.TransformPoint(vertex),Quaternion.identity,transform);
+                vertexHandle.vertexIndices.Add(i);
+                vertexHandle.meshFilter = meshFilter;
+                vertexHandleDictionary.Add(vertex,vertexHandle);
+            }
+            else
+            {
+                vertexHandleDictionary[vertex].vertexIndices.Add(i);
+            }
+            i++;
+        }
+        /*
+        for(int i = 0; i < meshFilter.mesh.triangles.Length; i += 3)
+        {
+            Vector3 point1 = meshFilter.mesh.vertices[meshFilter.mesh.triangles[i]];
+            Vector3 point2 = meshFilter.mesh.vertices[meshFilter.mesh.triangles[i+1]];
+            Vector3 point3 = meshFilter.mesh.vertices[meshFilter.mesh.triangles[i+2]];
+            Vector3 midpoint1 = Vector3.Lerp(point1,point2,0.5f);
+            Vector3 midpoint2 = Vector3.Lerp(point2,point3,0.5f);
+            Vector3 midpoint3 = Vector3.Lerp(point3,point1,0.5f);
+            
+            Instantiate(midpointHandlePrefab,meshFilter.transform.TransformPoint(midpoint1),Quaternion.identity);
+            Instantiate(midpointHandlePrefab,meshFilter.transform.TransformPoint(midpoint2),Quaternion.identity);
+            Instantiate(midpointHandlePrefab,meshFilter.transform.TransformPoint(midpoint3),Quaternion.identity);
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DiscGame/Scripts/VertexHandle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexHandle : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public List<int> vertexIndices = new List<int>();
+
+    private List<Vector3> vertexBuffer = new List<Vector3>();
+    private Vector3 lastLocalPosition;
+ 
+// call this if the mesh has been modified and you want to get the vertices.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastLocalPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.localPosition != lastLocalPosition)
+        {
+            vertexBuffer.Clear();
+            meshFilter.mesh.GetVertices(vertexBuffer);
+            foreach(int vertexIndex in vertexIndices)
+            {
+                vertexBuffer[vertexIndex] = transform.localPosition;
+            }
+            meshFilter.mesh.SetVertices(vertexBuffer);
+            lastLocalPosition = transform.localPosition;
+        }
+    }
+}
